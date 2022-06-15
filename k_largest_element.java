88968aea7b233399a9169f949e214b7805c034b1
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2022-06-16 00:18:34 +0100

    solve k largest using second method

--- k_largest_element.java
+++ k_largest_element.java
@@ -1,14 +1,32 @@
 import java.io.*;
 import java.math.*;
 import java.util.*;
+import java.util.stream.Collectors;
+
+// problem : given an array of integers arr
+// and integer k , find the kth largest
+// element
 
 class kLargestElement{
 
   public static int kLargest(int[] arr , int k ){
     int res = -1;
     //that's first brute first methode we sue nlog(n) in complexity
-    Arrays.sort(arr);
-    res = arr[k];
+    // Arrays.sort(arr);
+    // res = arr[k];
+
+    //other solution is to find maximum and remove it in each itteration
+      List<Integer> list =Arrays.stream(arr).boxed().collect(Collectors.toList());
+      for (int i=0;i<k ;k++ ) {
+      try{
+        int max =(int)Collections.max(list);
+      }catch(Exception ex){
+        System.out.println(ex.stack());
+      }
+      list.remove(Integer.valueOf(max));
+    }
+
+    res = Collections.max(list);
 
     return res;
   }
